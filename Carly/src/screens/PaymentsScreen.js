1555c367c5ad53508995eea546372083eac2196f
jdudzinska625 <jdudzinska625@gmail.com>
2023-12-30 17:39:40 +0100

    Payments screen added

--- Carly/src/screens/PaymentsScreen.js
+++ Carly/src/screens/PaymentsScreen.js
@@ -1,10 +1,24 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { styles } from '../styles';
+// PaymentsScreen.js
+import React from 'react';
+import { FlatList, View } from 'react-native';
+import PaymentItem from '../components/PaymentItem';
+import data from '../DummyData.json';
 
-export default function PaymentsScreen() {
-	return (
-		<View style={styles.screenContainer}>
-			<Text>Payments Screen</Text>
-		</View>
-	);
-}
+const PaymentsScreen = () => {
+  const payments = data.currentUser.payments;
+  const renderItem = ({ item }) => (
+    <PaymentItem amount={item.amount} date={item.date} type={item.type} />
+  );
+
+  return (
+    <View style={{ padding: 10, backgroundColor: 'white', flex: 1 }}>
+      <FlatList
+        data={payments}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+};
+
+export default PaymentsScreen;
