36e03bc2ff0e9d05a79653093cb9091733eddce4
jdudzinska625 <jdudzinska625@gmail.com>
2023-12-30 16:24:29 +0100

    Add dummy map

--- Carly/src/screens/MapScreen.js
+++ Carly/src/screens/MapScreen.js
@@ -1,11 +1,39 @@
-import { View, Text, StyleSheet } from "react-native";
-import { styles } from "../styles";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+// MapScreen.js
 
-function MapScreen({ navigation }) {
-    return <View style={styles.screenContainer}>
-      <Text>Map Screen</Text>
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+const MapScreen = () => {
+  // Replace with your initial region coordinates
+  const initialRegion = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} initialRegion={initialRegion}>
+        {/* Marker at the initial region */}
+        <Marker
+          coordinate={initialRegion}
+          title="Marker Title"
+          description="Marker Description"
+        />
+      </MapView>
     </View>
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
 
 export default MapScreen;
