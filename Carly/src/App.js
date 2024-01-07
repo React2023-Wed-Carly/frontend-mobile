6fbab03b87e66cb0466c64c4ee7c999af1bb004a
jdudz <jdudzinska625@gmail.com>
2024-01-07 22:17:54 +0100

    Add Settings Screen

--- Carly/src/App.js
+++ Carly/src/App.js
@@ -11,7 +11,7 @@ import AccountScreen from './screens/AccountScreen';
 import RentHistoryScreen from './screens/RentHistoryScreen';
 import FavoriteCarsScreen from './screens/FavoriteCarsScreen';
 import PaymentsScreen from './screens/PaymentsScreen';
-import SettingsScreen from './screens/SettinsScreen';
+import SettingsScreen from './screens/SettingsScreen';
 import MapScreen from './screens/MapScreen';
 import { styles } from './styles';
 import SelectedCarScreen from './screens/SelectedCarScreen';
@@ -20,37 +20,37 @@ const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
 function AppStack() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name='Book a Car' component={BookACarScreen} />
-      <Stack.Screen name='Selected Car' component={SelectedCarScreen} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			<Stack.Screen name="Book a Car" component={BookACarScreen} />
+			<Stack.Screen name="Selected Car" component={SelectedCarScreen} />
+		</Stack.Navigator>
+	);
 }
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName='Book a car'
-        drawerStyle={styles.drawerStyle}
-      >
-        <Drawer.Screen name='Home' component={HomeScreen} />
-        <Drawer.Screen name='Map' component={MapScreen} />
-        <Drawer.Screen name='Book a car' component={AppStack} />
-        <Drawer.Screen name='Book a flat' component={BookAFlatScreen} />
-        <Drawer.Screen name='Account' component={AccountScreen} />
-        <Drawer.Screen name='Rent history' component={RentHistoryScreen} />
-        <Drawer.Screen name='Favorite Cars' component={FavoriteCarsScreen} />
-        <Drawer.Screen name='Payments' component={PaymentsScreen} />
-        <Drawer.Screen name='Settings' component={SettingsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Drawer.Navigator
+				initialRouteName="Book a car"
+				drawerStyle={styles.drawerStyle}
+			>
+				<Drawer.Screen name="Home" component={HomeScreen} />
+				<Drawer.Screen name="Map" component={MapScreen} />
+				<Drawer.Screen name="Book a car" component={AppStack} />
+				<Drawer.Screen name="Book a flat" component={BookAFlatScreen} />
+				<Drawer.Screen name="Account" component={AccountScreen} />
+				<Drawer.Screen name="Rent history" component={RentHistoryScreen} />
+				<Drawer.Screen name="Favorite Cars" component={FavoriteCarsScreen} />
+				<Drawer.Screen name="Payments" component={PaymentsScreen} />
+				<Drawer.Screen name="Settings" component={SettingsScreen} />
+			</Drawer.Navigator>
+		</NavigationContainer>
+	);
 }
 
 export default registerRootComponent(App);
