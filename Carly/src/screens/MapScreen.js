d4e6680dec605a0f11c5d39494091756552aa11e
GitQueenZofia <zofia.anna.grabowska@gmail.com>
2024-01-08 21:28:26 +0100

    Map view in BookACarScreen

--- Carly/src/screens/MapScreen.js
+++ Carly/src/screens/MapScreen.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, View, TextInput, Button } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const MapScreen = ({ location, setLocation }) => {
   const apiKey = "AIzaSyBMARyYIchjEHKpN9Hz9PZKuMbPCngeAPs";
