ed993129dc2469b685b6ec432e42404f8264fa16
GitQueenZofia <zofia.anna.grabowska@gmail.com>
2024-01-11 19:46:52 +0100

    map view in BookACarScreen

--- Carly/src/screens/BookACarScreen.js
+++ Carly/src/screens/BookACarScreen.js
@@ -1,93 +1,119 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-	FlatList,
-	View,
-	TouchableOpacity,
-	Text,
-	Pressable,
-} from 'react-native';
-import jsonData from '../DummyData.json';
-import { styles } from '../styles';
-import BookCarItem from '../components/BookCarItem';
-import FilterScreen from './FilterScreen';
+  FlatList,
+  View,
+  TouchableOpacity,
+  Text,
+  Pressable,
+  Button,
+} from "react-native";
+import jsonData from "../DummyData.json";
+import { styles } from "../styles";
+import BookCarItem from "../components/BookCarItem";
+import FilterScreen from "./FilterScreen";
+import MapScreen from "./MapScreen";
 
 export default function BookACarScreen({ navigation }) {
-	const cars = jsonData.cars;
+  const cars = jsonData.cars;
 
-	const [isFilter, setIsFilter] = useState(false);
+  const [isFilter, setIsFilter] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [showCarList, setShowCarList] = useState(false);
 
-	const applyFilters = () => {
-		setIsFilter(false);
-	};
+  const toggleCarListView = () => {
+    setShowCarList((prev) => !prev);
+  };
+  const applyFilters = () => {
+    setIsFilter(false);
+  };
 
-	const openFilterScreen = () => {
-		setIsFilter(true);
-	};
+  const openFilterScreen = () => {
+    setIsFilter(true);
+  };
 
-	const handleCardPress = (car) => {
-		console.log(car);
-		navigation.push('Selected Car', { car });
-	};
+  const handleCardPress = (car) => {
+    navigation.push("Selected Car", { car });
+  };
 
-	return (
-		<View
-			style={{
-				padding: 20,
-				paddingHorizontal: 20,
-				backgroundColor: 'white',
-				flex: 1,
-			}}
-		>
-			{isFilter && (
-				<FilterScreen applyFilters={applyFilters} setIsFilter={setIsFilter} />
-			)}
-			{!isFilter && (
-				<View style={{ marginBottom: 40 }}>
-					<TouchableOpacity
-						style={bookStyles.filterButton}
-						onPress={openFilterScreen}
-					>
-						<Text style={bookStyles.buttonText}>Filter</Text>
-					</TouchableOpacity>
+  return (
+    <View
+      style={{
+        padding: 20,
+        paddingHorizontal: 20,
+        backgroundColor: "white",
+        flex: 1,
+      }}
+    >
+      <Button
+        title={showCarList ? "Show Map" : "Show Car List"}
+        onPress={toggleCarListView}
+      />
+      {showCarList && (
+        <>
+          {isFilter && (
+            <FilterScreen
+              applyFilters={applyFilters}
+              setIsFilter={setIsFilter}
+            />
+          )}
+          {!isFilter && (
+            <View style={{ marginBottom: 40 }}>
+              <TouchableOpacity
+                style={bookStyles.filterButton}
+                onPress={openFilterScreen}
+              >
+                <Text style={bookStyles.buttonText}>Filter</Text>
+              </TouchableOpacity>
 
-					<FlatList
-						data={cars}
-						keyExtractor={(item, index) => index.toString()}
-						renderItem={({ item }) => (
-							<Pressable key={item.id} onPress={() => handleCardPress(item)}>
-								<BookCarItem car={item} navigation={navigation} />
-							</Pressable>
-						)}
-					/>
-				</View>
-			)}
-		</View>
-	);
+              <FlatList
+                data={cars}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                  <Pressable
+                    key={item.id}
+                    onPress={() => handleCardPress(item)}
+                  >
+                    <BookCarItem car={item} navigation={navigation} />
+                  </Pressable>
+                )}
+              />
+            </View>
+          )}
+        </>
+      )}
+      {!showCarList && (
+        <MapScreen
+          location={currentLocation}
+          setLocation={setCurrentLocation}
+        />
+      )}
+    </View>
+  );
 }
 
 export const bookStyles = {
-	filterButton: {
-		backgroundColor: '#dcdcdc', // Light grey color
-		padding: 10,
-		borderRadius: 20, // More rounded edges
-		marginVertical: 10,
-		marginTop: 0,
-		alignItems: 'center',
-		shadowColor: '#000',
-		shadowOffset: { width: 0, height: 2 },
-		shadowOpacity: 0.2,
-		shadowRadius: 2,
-		elevation: 2,
-	},
-	buttonText: {
-		color: '#000',
-		fontSize: 16,
-	},
-	cardContainer: {
-		backgroundColor: '#ecf0f1',
-		padding: 20,
-		marginVertical: 10,
-		borderRadius: 10,
-	},
-	// Add other styles as needed
+  filterButton: {
+    backgroundColor: "#dcdcdc", // Light grey color
+    padding: 10,
+    borderRadius: 20, // More rounded edges
+    marginVertical: 10,
+    marginTop: 0,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 16,
+  },
+  cardContainer: {
+    backgroundColor: "#ecf0f1",
+    padding: 20,
+    marginVertical: 10,
+    borderRadius: 10,
+  },
+  // Add other styles as needed
 };
--- Carly/src/screens/MapScreen.js
+++ Carly/src/screens/MapScreen.js
@@ -1,28 +1,15 @@
-// MapScreen.js
-
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, TextInput, Button } from "react-native";
-import { Picker } from "@react-native-picker/picker";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import jsonData from "../DummyData.json";
 
-const MapScreen = () => {
-  // Replace with your initial region coordinates
-  // const initialRegion = {
-  //   latitude: 37.78825,
-  //   longitude: -122.4324,
-  //   latitudeDelta: 0.0922,
-  //   longitudeDelta: 0.0421,
-  // };
+const MapScreen = ({ location, setLocation }) => {
   const cars = jsonData.cars;
 
-  const [currentLocation, setCurrentLocation] = useState(null);
   const [initialRegion, setInitialRegion] = useState(null);
-  const [unit, setUnit] = useState("km");
-  const [maxDistance, setMaxDistance] = useState(10);
-  const [carsInRadius, setCarsInRadius] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState(null);
 
   useEffect(() => {
     const getLocation = async () => {
@@ -31,10 +18,8 @@ const MapScreen = () => {
         console.log("Permission to access location was denied");
         return;
       }
-
       let location = await Location.getCurrentPositionAsync({});
       setCurrentLocation(location.coords);
-
       setInitialRegion({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
@@ -42,90 +27,45 @@ const MapScreen = () => {
         longitudeDelta: 0.005,
       });
     };
-
     getLocation();
   }, []);
 
-  const getCarsInRadius = () => {
-    if (!currentLocation) {
-      return;
-    }
-    const radius = unit === "km" ? 6371 : 6371000;
+  const handleMapPress = (event) => {
+    setSelectedLocation(event.nativeEvent.coordinate);
+  };
 
-    const filteredCars = cars.filter((car) => {
-      const distance = calculateDistance(
-        currentLocation.latitude,
-        currentLocation.longitude,
-        car.location.latitude,
-        car.location.longitude,
-        radius
-      );
-      return distance <= maxDistance;
+  const handleAddressSelect = (data, details) => {
+    setSelectedLocation({
+      latitude: details.geometry.location.lat,
+      longitude: details.geometry.location.lng,
     });
-
-    setCarsInRadius(filteredCars);
-  };
-
-  const calculateDistance = (lat1, lon1, lat2, lon2,radius) => {
-    const dLat = deg2rad(lat2 - lat1);
-    const dLon = deg2rad(lon2 - lon1);
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(deg2rad(lat1)) *
-        Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = radius * c; // Odległość w km
-    return distance;
-  };
-  const deg2rad = (deg) => {
-    return deg * (Math.PI / 180);
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.searchContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder={`Max distance (${unit})`}
-          keyboardType="numeric"
-          onChangeText={(text) => setMaxDistance(text)}
-        />
-        <Button title="Search" onPress={getCarsInRadius} />
-      </View>
-
-      <Picker
-        selectedValue={unit}
-        onValueChange={(itemValue, itemIndex) => setUnit(itemValue)}
+      <MapView
+        style={styles.map}
+        initialRegion={initialRegion}
+        onPress={handleMapPress}
       >
-        <Picker.Item label="meters" value="m" />
-        <Picker.Item label="kilometers" value="km" />
-      </Picker>
-
-      {initialRegion && (
-        <MapView style={styles.map} initialRegion={initialRegion}>
-          {currentLocation && (
-            <Marker
-              coordinate={{
-                latitude: currentLocation.latitude,
-                longitude: currentLocation.longitude,
-              }}
-              title="Your Location"
-            />
-          )}
-          {carsInRadius.map((vehicle) => (
-            <Marker
-              key={vehicle.id}
-              coordinate={{
-                latitude: vehicle.location.latitude,
-                longitude: vehicle.location.longitude,
-              }}
-              title="marker"
-            />
-          ))}
-        </MapView>
-      )}
+        {currentLocation && (
+          <Marker
+            coordinate={{
+              latitude: currentLocation.latitude,
+              longitude: currentLocation.longitude,
+            }}
+            title="Your Location"
+          />
+        )}
+        {selectedLocation && (
+          <Marker
+            coordinate={selectedLocation}
+            title="Selected Location"
+            draggable
+            onDragEnd={(e) => setSelectedLocation(e.nativeEvent.coordinate)}
+          />
+        )}
+      </MapView>
     </View>
   );
 };
@@ -136,23 +76,6 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
-  },
-  searchContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 10,
-    backgroundColor: "#fff",
-  },
-  input: {
-    flex: 1,
-    marginRight: 10,
-    borderWidth: 1,
-    padding: 8,
-  },
-  unitButtons: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginTop: 10,
   },
 });
 
